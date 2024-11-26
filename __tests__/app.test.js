78896de18cf5400d9fafa050cc5b6de4b0774dc1
biblically-approximate-arda <ardacaglar1@gmail.com>
2024-11-26 22:03:49 +0000

    3-get-api-topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,4 +1,5 @@
 const endpointsJson = require("../endpoints.json");''
+const topicsJson = require("../db/data/development-data/topics")
 /* Set up your test imports here */
 const request = require("supertest")
 const app = require("./../db/app")
@@ -12,6 +13,17 @@ describe("GET /api", () => {
       .expect(200)
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects, each with a slug and a description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics).toEqual(topicsJson);
       });
   });
 });
