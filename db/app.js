b6698df4d7ce88aeeaab3987e726f543edb2736f
biblically-approximate-arda <ardacaglar1@gmail.com>
2024-11-27 19:44:25 +0000

    4-get-article-by-id

--- db/app.js
+++ db/app.js
@@ -1,12 +1,13 @@
 const exp = require("express")
 const app = exp()
-const {getApi, getApiTopics} = require("./controller")
+const {getApi, getApiTopics, getArticleById} = require("./controller")
 module.exports = app
 
 app.use(exp.json())
 
 app.get("/api", getApi)
 app.get("/api/topics", getApiTopics)
+app.get("/api/articles/:article_id", getArticleById)
 
 app.use((req, res, next) => {
   console.log("ping")
--- db/controller.js
+++ db/controller.js
@@ -5,5 +5,13 @@ module.exports.getApi = function(req, res) {
 }
 
 module.exports.getApiTopics = function(req, res) {
-    res.status(200).send({topics: model.topics})
+  res.status(200).send({topics: model.topics})
+}
+
+module.exports.getArticleById = function(req, res) {
+  model.fetchArticleById(req.params.article_id)
+  .then(({rows}) => {
+    if (rows.length) res.status(200).send({article: rows[0]})
+    else res.status(404).send({msg: "No such article"})
+  })
 }
